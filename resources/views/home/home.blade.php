4d125f4c871e415b1ef7a3dd94c51f0d56dd07e7
Kamil Rogaczewski <k.rogaczewski@hotmail.com>
2023-01-08 22:39:58 +0100

    login-logout

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.layout')
+@section('content')
+    <main id="main" class="main">
+        <div class="pagetitle">
+            <h1>Dashboard</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ol>
+            </nav>
+        </div>
+        <section class="section dashboard">
+
+            AAAAAAAAAAAAAAAAA
+
+        </section>
+    </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +12,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('hom');
