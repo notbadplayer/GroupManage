c5bf2e4cbe27c9cff8f5bb3cdc5b2669041d2f24
Kamil Rogaczewski <k.rogaczewski@hotmail.com>
2023-01-08 20:02:09 +0100

    add template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+
+//template js:
+import './apexcharts.min';
+import './chart.min';
+import './echarts.min';
+import './main';
+//import './quill.min';
+import './simple-datatables';
+import './tinymce.min';
+import './validate';
