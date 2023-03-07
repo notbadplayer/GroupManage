fd0dab9cebf90df65e699cadaa780c2ef06fbf2a
Kamil Rogaczewski <notbadplayer@gmail.com>
2023-03-07 14:41:02 +0100

    change mail configuration

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'jfstudio@jfstudio.pl'),
-        'name' => env('MAIL_FROM_NAME', 'JFstudio'),
+        'address' => env('MAIL_FROM_ADDRESS', 'jfstudiosuprasl@gmail.com'),
+        'name' => env('MAIL_NAME', 'JFstudio'),
     ],
 
     /*
