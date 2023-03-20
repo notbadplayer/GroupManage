304ff826c6b782ddd986c99c2a13ca94eb9cad19
Kamil Rogaczewski <notbadplayer@gmail.com>
2023-03-20 13:58:24 +0100

    fix recently added note

--- resources/views/home/recentAddedNotes.blade.php
+++ resources/views/home/recentAddedNotes.blade.php
@@ -1,11 +1,11 @@
-@if(isset($lastNote))
+@if(isset($latestNote))
 <div class="card info-card revenue-card">
     <div class="card-body">
         <h5 class="card-title">Ostatnio dodane nuty</h5>
         <div class="d-flex align-items-center">
             <div class="card-icon rounded-circle d-flex align-items-center justify-content-center"> <i
                     class="fa-brands fa-itunes-note"></i></div>
-            <a href="/file-download/note/{{ $latestNote->id  }}">
+            <a href="/file-download/Note/{{ $latestNote->id  }}">
                 <div class="ps-3">
                     <h6>{{ $latestNote->name }}</h6>
                     <span class="text-muted small pt-2 ps-1">Kliknij aby pobrać</span>
